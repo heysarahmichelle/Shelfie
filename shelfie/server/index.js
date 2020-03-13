95233c97efec5487f07ebca8464e065e7b2d0909
sarahcrox <croxford.sarahm@gmail.com>
2020-03-13 10:15:23 -0600

    Database Completed

--- shelfie/server/index.js
+++ shelfie/server/index.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-
+const massive = require('massive');
 const app = express();
-
 const ctrl = require('./controller');
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
+
 
 app.use(express.json())
 
-const PORT = 5500;
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+})
+.then(db => {
+    app.set('db', db)
+    console.log('DB Connected')
+})
 
-app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`));
+const PORT = 5500;
+app.listen(SERVER_PORT, () => console.log(`SERVER RUNNING ON PORT ${SERVER_PORT}`));
