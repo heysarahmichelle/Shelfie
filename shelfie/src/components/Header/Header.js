801b1071b31c051d438990a68fcb8caad5a09fa5
sarahcrox <croxford.sarahm@gmail.com>
2020-03-13 09:44:05 -0600

    React Section Completed

---
+++ shelfie/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                Header Component
+            </div>
+        )
+    }
+}
+
+  export default Header;
---
+++ shelfie/src/components/Product/Product.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Product extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                Product Component
+            </div>
+        )
+    }
+}
+
+  export default Product;
